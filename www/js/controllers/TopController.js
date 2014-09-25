49c9d51c2cf6a8fb4554ac1230df95f6225f9e2a
zoei@163.com <zoei@163.com>
2014-09-26 01:54:51 +0800

    mod carousel

--- www/js/controllers/TopController.js
+++ www/js/controllers/TopController.js
@@ -1,13 +1,35 @@
 namespace('App.controller');
 App.controller.TopCtrl = ez.base.BaseController.extend({
-	$inject: ['$scope', 'TopFact'],
-	init: function($scope, TopFact) {
-		$scope.topInfo = TopFact.get(function(info){
-			// if($scope.carousel){
-			// 	$scope.carousel.setData(info.slides);
-			// }
-			$scope.cdata = info.slides;
-			$scope.cdata2 = info.slides.slice(0, 2);
-		});
+	$inject: ['$scope', 'TopFact', 'DianPingApi'],
+	init: function($scope, TopFact, DianPingApi) {
+		var me = this;
+
+		var searchBusiness = function(){
+			var params = {};
+			params.region = '黄浦区';
+			params.category = '美食';
+			params.limit = 5;
+
+			DianPingApi.getBusiness(params, function(result){
+				var cdata = [];
+				for (var i=0;i<result.length;i++) {
+					var item = result[i];
+					cdata.push({
+						title: me.formatName(item.name),
+						image: item.icon,
+						text: item.address,
+						class: '',
+					});
+				};
+				cdata[0].class = ' active';
+				$scope.cdata = cdata;
+			});
+		};
+		searchBusiness();
+	},
+
+	formatName: function(name) {
+		/(.*)\(.*\)/.test(name);
+		return RegExp.$1 || name;
 	}
 });
--- www/js/ez/directives/Carousel.js
+++ www/js/ez/directives/Carousel.js
@@ -4,18 +4,20 @@ namespace('ez.directives').Carousel = ez.base.BaseDirective.extend({
 	restrict: 'EA',
 	templateUrl: 'partials/directives/carousel.html',
 	replace: true,
-	scope: true,
+	scope: {
+		dataName: '@data'
+	},
 	link: function(scope, element, attrs) {
 		scope.carouselId = attrs['identity'] || 'carousel-'+ Math.uuid(8);
-		var dataName = attrs['data'] || 'slides';
 
 		scope.itemChangeEventName = attrs['itemChange'] || 'itemChange';
 
-		scope.slides = scope.$parent[dataName];
-		scope.carousel = {
-			setData: function(data){
-				scope.slides = data;
-			},
+		scope.$parent.$watch(scope.dataName, function(d){
+			scope.slides = d;
+		});
+
+		scope.setData = function(data){
+			scope.slides = data;
 		};
 	}
 });
